57334faa0ef5eb9d8a151aaf1d10728cceeda6f4
Morten <hulvei3@gmail.com>
2011-10-07 17:57:31 +0200

    udbygget playeren, der nu kan afspille/stoppe musik fra lokal fil. Desuden vises tiden i GUI

--- MusicStreamer/MainWindow.xaml.cs
+++ MusicStreamer/MainWindow.xaml.cs
@@ -19,9 +19,45 @@ namespace MusicStreamer
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void buttonPlay_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerEngine.instance.loadFile("C:\\Users\\Morten Hulvej\\Desktop\\07 I Remember.mp3");
+
+            PlayerEngine.instance.playFile();
+            
+        }
+
+        private void buttonStop_Click(object sender, RoutedEventArgs e)
+        {
+            PlayerEngine.instance.stopFile();
+
+        }
+
+        private void buttonConnect_Click(object sender, RoutedEventArgs e)
+        {
+            int time = (int)PlayerEngine.instance.CurrentTime;
+
+
+            int hours = time / 3600;
+            int minutes = time / 60;
+            int seconds = time % 60;
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append( (hours < 10) ? "0" + hours : hours.ToString() );
+            sb.Append(":");
+            sb.Append( (minutes < 10) ? "0" + minutes : minutes.ToString() );
+            sb.Append(":");
+            sb.Append((seconds < 10) ? "0" + seconds : seconds.ToString() );
+
+            textBoxTime.Text = sb.ToString();
+            //textBoxTime.Text = time / 3600 + ":" + time / 60 + ":" + time % 60;
         }
     }
 }
--- MusicStreamer/PlayerEngine.cs
+++ MusicStreamer/PlayerEngine.cs
@@ -2,11 +2,104 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using WMPLib;
 
 namespace MusicStreamer
 {
     class PlayerEngine
     {
+        private WindowsMediaPlayer mediaPlayer = null;
+        private static PlayerEngine player = null;
+
+        // singleton 
+        public static PlayerEngine instance
+        {
+            get
+            {
+                if (player == null)
+                    player = new PlayerEngine();
+                return player;
+            }
+        }
+
+        // gets remaining time of current song (in seconds)
+        public double RemainingTime
+        {
+            get { if (mediaPlayer != null)
+                       return Math.Floor(mediaPlayer.currentMedia.duration - mediaPlayer.controls.currentPosition);
+                  return 0;
+            }
+        }
+
+        // gets/sets current duration of current song (in seconds)
+        public double CurrentTime
+        {
+            get
+            {
+                if (mediaPlayer != null)
+                    return mediaPlayer.controls.currentPosition;
+                return 0;
+            }
+            set
+            {
+                if (mediaPlayer != null)
+                    mediaPlayer.controls.currentPosition = value;
+            }
+        }
+
+        // checks to see if the player is initiated
+        private Boolean PlayerInitiated
+        {
+            get { return mediaPlayer != null; }
+        }
+
+        private PlayerEngine()
+        {
+            mediaPlayer = new WindowsMediaPlayer();
+            mediaPlayer.settings.autoStart = false;
+        }
+        
+        // setting current song to the file from 'filepath'
+        public void loadFile(String filepath)
+        {
+            if (mediaPlayer == null)
+                return;
+
+            mediaPlayer.URL = filepath;
+
+            
+        }
+
+        // plays the file from the 'URL'-property
+        public void playFile()
+        {
+            if (mediaPlayer == null)
+                return;
+
+           
+            mediaPlayer.controls.play();
+            
+        }
+
+        // pauses the current song
+        public void pauseFile()
+        {
+            mediaPlayer.controls.pause();
+        }
+
+        // stops the current song
+        public void stopFile()
+        {
+            mediaPlayer.controls.stop();
+        }
+
+        public void closePlayer()
+        {
+            mediaPlayer.close();
+            mediaPlayer = null;
+            player = null;
+        }
+            
 
     }
 }
