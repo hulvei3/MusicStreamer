84a09f4b89c884009e16505f9c3894d8c2f1ae9c
harving <stmharving@gmail.com>
2011-12-05 21:56:15 +0100

    NU VIRKER DET!

--- MusicStreamer/CustomCommands/SavePlaylistCommand.cs
+++ MusicStreamer/CustomCommands/SavePlaylistCommand.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
+using MusicStreamer.ViewModels;
+using System.Windows;
 
 namespace MusicStreamer.CustomCommands
 {
-    class SavePlaylistCommand
+    class SavePlaylistCommand : ICommand
     {
+        private PlaylistViewModel _plvm;
+
+        public SavePlaylistCommand(PlaylistViewModel plvm)
+        {
+            this._plvm = plvm;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            _plvm.SavePlaylist();
+            MessageBox.Show("Playlist saved!");
+        }
     }
 }
