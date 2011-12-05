28a1c03ccd0d1b7b73e6dc33738678b4f60f0882
harving <stmharving@gmail.com>
2011-12-05 21:56:15 +0100

    NU VIRKER DET!

---
+++ FtpTest/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MusicStreamer;
+using System.Net;
+using System.IO;
+
+
+namespace FtpTest
+{
+    class Program
+    {
+        private FtpWebRequest Request { get; set; }
+
+        private long remFileSize { get; set; }
+
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            prog.run();
+
+        }
+
+        public void run()
+        {
+            String user = "streamer";
+            String pw = "streamerpassword";
+            String host = "ftp://90.184.75.15/OpenShare/Music/classical.WAV";
+
+            //Console.WriteLine(host, user, pw);
+
+            //Request = (FtpWebRequest)WebRequest.Create(host);
+
+            //Request.Credentials = new NetworkCredential(user, pw);
+            //Request.UseBinary = true;
+            //Request.Method = null;
+            ////Request.Method = WebRequestMethods.Ftp.GetFileSize;
+            //Request.Proxy = null;
+
+
+            //FtpWebResponse response = (FtpWebResponse)Request.GetResponse();
+            //response = (FtpWebResponse)Request.GetResponse();
+            //remFileSize = response.ContentLength;
+            //Stream responseStream = response.GetResponseStream();
+            //StreamReader reader = new StreamReader(responseStream);
+            //String s = reader.ReadToEnd();
+            //long size = remFileSize;
+            //Console.WriteLine(size);
+            //Console.WriteLine(s);
+
+            //string q = Console.ReadLine();
+
+            //MusicStreamer.ViewModels.Server.ServerNavigationViewModel snvm = new MusicStreamer.ViewModels.Server.ServerNavigationViewModel();
+
+            WebClient wc = new WebClient();
+            wc.Credentials = new NetworkCredential(user, pw);
+
+            wc.OpenRead("ftp://90.184.75.15/OpenShare/Music/classical.WAV");
+
+
+            string bytes_total = wc.ResponseHeaders["Content-Length"];
+            Console.WriteLine(bytes_total + " Bytes");
+            string q = Console.ReadLine();
+
+        }
+
+        
+    }
+}
--- MusicStreamer/CustomCommands/SavePlaylistCommand.cs
+++ MusicStreamer/CustomCommands/SavePlaylistCommand.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
+using MusicStreamer.ViewModels;
 
 namespace MusicStreamer.CustomCommands
 {
-    class SavePlaylistCommand
+    class SavePlaylistCommand : ICommand
     {
+        private MainWindowViewModel _mwvm;
+
+        public SavePlaylistCommand(MainWindowViewModel mwvm)
+        {
+            this._mwvm = mwvm;
+        }
+        public bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+
+            throw new NotImplementedException();
+        }
     }
 }
