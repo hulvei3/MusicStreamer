5ecb92eb3724cd0a2016a1c5de53818e6ff109a4
Morten <hulvei3@gmail.com>
2011-12-15 23:54:31 +0100

    Server/ServerlistItemViewModel.cs added again (dont ask)

---
+++ MusicStreamer/ViewModels/Server/ServerlistItemViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MusicStreamer.ViewModels.Server
+{
+    class ServerlistItemViewModel : INotifyPropertyChanged
+    {
+        private string _url;
+        private string _size;
+
+        public ServerlistItemViewModel(string name, string size)
+        {
+            Url = name;
+            Name = name;
+            Size = size;
+        }
+
+        public string Url
+        {
+            get { return _url; }
+            set { _url = value; }
+        }
+        public string Name { get; set; }
+
+        public string Size
+        {
+            get { return _size; }
+            set { _size = value; }
+        }
+        public RoutedCommand AddCommand{ get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
